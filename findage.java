523314762ddc9fc0b85b5dfbaae07644eb25b03a
Kavya.v <kavyapatil2257@gmail.com>
2025-07-01 18:38:44 +0530

    added voter

--- findage.java
+++ findage.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 class findage{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("enter your age ");
         int age=sc.nextInt();
         if(age<=18){
             System.out.println("not eligible to vote");
